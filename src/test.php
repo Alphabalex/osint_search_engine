c0d08a1cb15777eeacdf1922503852ca2bad0d74
Balogun Abdulquddus <babusunnah@gmail.com>
2025-01-31 18:06:21 +0100

    advanced search

--- src/test.php
+++ src/test.php
@@ -3,8 +3,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Eaglewatch\SearchEngines\Pipl;
 
+// advanced search
+$fields = array(
+    new PiplApi_Address(array("country" => "US", "state" => "KS", "city" => "Metropolis")),
+    new PiplApi_Address(array("country" => "US", "state" => "KS", "city" => "Smallville")),
+    new PiplApi_Name(array("first" => "Clark", "middle" => "Joseph", "last" => "Kent")),
+    new PiplApi_Job(array("title" => "Field Reporter")),
+);
+$person = new PiplApi_Person($fields);
+$search = array('person' => $person);
 
-$search = array('email' => 'clark.kent@example.com', 'first_name' => 'Clark','last_name' => 'Kent');   
+$search = array('email' => 'clark.kent@example.com', 'first_name' => 'Clark', 'last_name' => 'Kent');
 $pipl = new Pipl();
 $response = $pipl->search($search);
 
